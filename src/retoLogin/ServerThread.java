3aafe0235ec15a0778e2884be9709a4efa4b0306
Unai Pérez Sánchez <unain@DESKTOP-NPUAMKA>
2019-10-25 13:54:18 +0200

    Server finnished

--- src/retoLogin/ServerThread.java
+++ src/retoLogin/ServerThread.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import retoLogin.control.DataAccess;
 import retoLogin.control.DataAccessFactory;
